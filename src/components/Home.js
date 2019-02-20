1a6acc732e8525fe47eefc5bcfaa0e47f3a2c388
Will <megapocketninja@gmail.com>
2019-02-20 16:06:56 -0500

    updated gallery styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,15 +6,10 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <div>
-                    <p>About Me Blurb</p>
-                </div>
-                <div>
-                    <h1>Hey There! My name is William, but you can call me Will :)</h1>
-                    <h2>I'm a web developer from Atlanta, GA.</h2>
+                <h1>Hey There! My name is William, but you can call me Will :)</h1>
+                <h2>I'm a web developer from Atlanta, GA.</h2>
 
-                </div>
-            </div >
+            </div>
         );
     }
 }
--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -2,23 +2,68 @@ import React, { Component } from 'react';
 import ProjectTile from './ProjectTile';
 import makeCarousel from 'react-reveal/makeCarousel'
 import Slide from 'react-reveal/Slide'
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 const StyledGallery = styled.div`
 display: flex;
 flex-direction: column;
 align-content: space-around;
 `
+const width = '70vw', height = '75vh';
 const Container = styled.div`
   border: 1px solid black;
   position: relative;
   overflow: hidden;
-  width: 70vw;
-  height: 75vh;
+  width: ${width};
   margin: 0 auto;
-`
-
-const CarouselUI = ({ children }) => <Container>{children}</Container>;
+  display:flex;
+  flex-direction: column;
+`;
+const Children = styled.div`
+  width: ${width};
+  position: relative;
+  height: ${height};
+`;
+const Arrow = styled.div`
+  text-shadow: 1px 1px 1px #fff;
+  z-index: 100;
+  line-height: ${height};
+  text-align: center;
+  position: absolute;
+  top: 0;
+  width: 10%;
+  font-size: 3em;
+  cursor: pointer;
+  user-select: none;
+  ${props => props.right ? css`left: 90%;` : css`left: 0%;`}
+`;
+const Dot = styled.span`
+  font-size: 1.5em;
+  cursor: pointer;
+  text-shadow: 1px 1px 1px #fff;
+  user-select: none;
+`;
+const Dots = styled.span`
+  justify-self: center;
+  text-align: center;
+  width: ${width};
+  z-index: 100;
+`;
+const CarouselUI = ({ position, total, handleClick, children }) => (
+    <Container>
+        <Children>
+            {children}
+            <Arrow onClick={handleClick} data-position={position - 1}>{'{'}</Arrow>
+            <Arrow right onClick={handleClick} data-position={position + 1}>{'}'}</Arrow>
+        </Children>
+        <Dots>
+            {Array(...Array(total)).map((val, index) =>
+                <Dot key={index} onClick={handleClick} data-position={index}>
+                    {index === position ? '● ' : '○ '}
+                </Dot>
+            )}
+        </Dots>
+    </Container>)
 const Carousel = makeCarousel(CarouselUI);
 
 class ProjectGallery extends Component {
@@ -41,7 +86,7 @@ class ProjectGallery extends Component {
         return (
             <StyledGallery>
                 <h1>Projects</h1>
-                <Carousel defaultWait={3000} maxTurns={20}>
+                <Carousel defaultWait={3000} maxTurns={6}>
                     {this.state.projects.map((project, i) => {
                         return (<Slide right key={i}><ProjectTile
                             key={i}
