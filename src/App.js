f5e3d51303f5390264ac153fac18e0fe171cb413
Will <megapocketninja@gmail.com>
2019-02-20 00:35:45 -0500

    Added components and inital seed data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Home from "./components/Home"
+import NavBar from "./components/NavBar"
+import Resume from "./components/Resume"
+import About from "./components/About"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home />
-      </div>
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/resume" component={Resume} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
