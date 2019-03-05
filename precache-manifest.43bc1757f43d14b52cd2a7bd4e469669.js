281afe91847078ec241078667e8d52a3938b7a70
Will <megapocketninja@gmail.com>
2019-03-05 10:28:49 -0500

    Updates

---
+++ precache-manifest.43bc1757f43d14b52cd2a7bd4e469669.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "297c9f76e5aa49484bec",
+    "url": "/react-portfolio/static/css/main.20ed6e0f.chunk.css"
+  },
+  {
+    "revision": "297c9f76e5aa49484bec",
+    "url": "/react-portfolio/static/js/main.297c9f76.chunk.js"
+  },
+  {
+    "revision": "1e4ad457277730ac33c8",
+    "url": "/react-portfolio/static/js/runtime~main.1e4ad457.js"
+  },
+  {
+    "revision": "d2257394024d2aa232bf",
+    "url": "/react-portfolio/static/js/2.d2257394.chunk.js"
+  },
+  {
+    "revision": "2866959d7a4d59e6095d5a50bdba0ea5",
+    "url": "/react-portfolio/static/media/simon-desktop.2866959d.png"
+  },
+  {
+    "revision": "16ad428e91cec8111f3d9b35b8573434",
+    "url": "/react-portfolio/static/media/squadapp.16ad428e.png"
+  },
+  {
+    "revision": "87d80ab5b1abfcbc4988307805365b92",
+    "url": "/react-portfolio/static/media/festpack.87d80ab5.png"
+  },
+  {
+    "revision": "85be037fd07a4fcb75e0b22241047cb2",
+    "url": "/react-portfolio/index.html"
+  }
+];
